b5a37ea403a6f8730554d816d3438f59b70a9096
chusanapak fongmanee <141717296+spritermutt@users.noreply.github.com>
2025-12-04 18:03:26 +0700

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => {
+      return !!token
+    },
+  },
+})
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|$).*)",
+  ],
+}
